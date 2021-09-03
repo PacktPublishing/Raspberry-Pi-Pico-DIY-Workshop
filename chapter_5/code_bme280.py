09f44415dd732f7bff200c15fedc6819cc7d2f66
Sai Yamanoor <54855965+sai-ydev@users.noreply.github.com>
2021-09-02 23:43:36 -0400

    Chapter 5 files

---
+++ chapter_5/code_bme280.py
@@ -0,0 +1,18 @@
+import board
+import busio
+import time
+from adafruit_bme280 import basic as adafruit_bme280
+
+# Create sensor object, using the board's default I2C bus.
+i2c = busio.I2C(board.SCL, board.SDA)   # uses board.SCL and board.SDA
+bme280 = adafruit_bme280.Adafruit_BME280_I2C(i2c)
+
+# change this to match the location's pressure (hPa) at sea level
+bme280.sea_level_pressure = 1013.25
+
+while True:
+    print("\nTemperature: %0.1f C" % bme280.temperature)
+    print("Humidity: %0.1f %%" % bme280.relative_humidity)
+    print("Pressure: %0.1f hPa" % bme280.pressure)
+    print("Altitude = %0.2f meters" % bme280.altitude)
+    time.sleep(2)

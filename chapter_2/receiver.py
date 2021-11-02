991b8b39bd4021ed01ae4806263aabae35df2199
Sai Yamanoor <54855965+sai-ydev@users.noreply.github.com>
2021-11-02 16:52:36 -0400

    Update receiver.py

--- chapter_2/receiver.py
+++ chapter_2/receiver.py
@@ -5,8 +5,10 @@ import busio
 uart = busio.UART(board.GP0, board.GP1, baudrate=9600)
 
 while True:
+    # await incoming message
     bytes_waiting = uart.in_waiting
     if bytes_waiting:
         incoming_msg = uart.readline()
         print(incoming_msg)
+        # re-transmit
         uart.write(incoming_msg)

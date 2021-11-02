c3bd294ed038fb9528bf01689faaa1a598598b87
Sai Yamanoor <54855965+sai-ydev@users.noreply.github.com>
2021-11-02 00:05:21 -0400

    Update code_displaytemp.py

--- chapter_2/code_displaytemp.py
+++ chapter_2/code_displaytemp.py
@@ -7,22 +7,20 @@ from adafruit_display_text import label
 import adafruit_displayio_ssd1306
 from adafruit_htu21d import HTU21D
 
-
 displayio.release_displays()
 
-oled_reset = board.GP12
-
-# Use for SPI
 spi = busio.SPI(board.GP2, MOSI=board.GP3)
 i2c = busio.I2C(board.GP9, board.GP8)
 
 oled_cs = board.GP11
 oled_dc = board.GP13
+oled_reset = board.GP12
+
 display_bus = displayio.FourWire(spi, command=oled_dc, chip_select=oled_cs,
                                 reset=oled_reset, baudrate=1000000)
 
 WIDTH = 128
-HEIGHT = 32  # Change to 64 if needed
+HEIGHT = 32
 
 sensor = HTU21D(i2c)
 display = adafruit_displayio_ssd1306.SSD1306(display_bus, width=WIDTH, height=HEIGHT)

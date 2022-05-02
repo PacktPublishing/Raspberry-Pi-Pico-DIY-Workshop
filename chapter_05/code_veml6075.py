ef194b38325a1645989829dec46e0e8c80c8cfb6
Sai Yamanoor <54855965+sai-ydev@users.noreply.github.com>
2022-05-01 23:40:46 -0400

    Update code_veml6075.py

--- chapter_05/code_veml6075.py
+++ chapter_05/code_veml6075.py
@@ -8,5 +8,5 @@ i2c = busio.I2C(board.SCL, board.SDA)
 veml = adafruit_veml6075.VEML6075(i2c, integration_time=100)
 
 while True:
-    print(veml.uv_index)
+    print("UV Index: " + str(veml.uv_index))
     time.sleep(1)

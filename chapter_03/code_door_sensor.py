93e6d3f6bce7a472b5949dd477a7c6d60573e084
Sai Yamanoor <54855965+sai-ydev@users.noreply.github.com>
2022-05-01 22:28:05 -0400

    Chapter 3 files

--- chapter_03/code_door_sensor.py
+++ chapter_03/code_door_sensor.py
@@ -2,11 +2,13 @@ import board
 import digitalio
 import time
 
-switch = digitalio.DigitalInOut(board.D12)
+switch = digitalio.DigitalInOut(board.GP12)
 switch.direction = digitalio.Direction.INPUT
 switch.pull = digitalio.Pull.UP
 
 while True:
     print(switch.value)
+    if not switch.value:
+        print("Door Sensor activated")
     time.sleep(1)
 
---
+++ chapter_03/code_publish_io.py
@@ -0,0 +1,65 @@
+import board
+import busio
+from digitalio import DigitalInOut
+import adafruit_requests as requests
+import adafruit_esp32spi.adafruit_esp32spi_socket as socket
+from adafruit_esp32spi import adafruit_esp32spi
+from adafruit_io.adafruit_io import IO_HTTP, AdafruitIO_RequestError
+from adafruit_htu21d import HTU21D
+
+i2c = busio.I2C(board.GP9, board.GP8)
+sensor = HTU21D(i2c)
+
+# Get wifi details and more from a secrets.py file
+try:
+    from secrets import secrets
+except ImportError:
+    print("WiFi secrets are kept in secrets.py, please add them there!")
+    raise
+
+#  ESP32 pins
+esp32_cs = DigitalInOut(board.GP7)
+esp32_ready = DigitalInOut(board.GP10)
+esp32_reset = DigitalInOut(board.GP11)
+
+spi = busio.SPI(board.GP18, board.GP19, board.GP16)
+esp = adafruit_esp32spi.ESP_SPIcontrol(spi, esp32_cs, esp32_ready, esp32_reset)
+
+requests.set_socket(socket, esp)
+
+if esp.status == adafruit_esp32spi.WL_IDLE_STATUS:
+    print("ESP32 found and in idle mode")
+print("Firmware vers.", esp.firmware_version)
+print("MAC addr:", [hex(i) for i in esp.MAC_address])
+
+print("Connecting to AP...")
+while not esp.is_connected:
+    try:
+        esp.connect_AP(secrets["ssid"], secrets["password"])
+    except RuntimeError as e:
+        print("could not connect to AP, retrying: ", e)
+        continue
+print("Connected to", str(esp.ssid, "utf-8"), "\tRSSI:", esp.rssi)
+print("My IP address is", esp.pretty_ip(esp.ip_address))
+
+socket.set_interface(esp)
+requests.set_socket(socket, esp)
+
+aio_username = secrets["aio_username"]
+aio_key = secrets["aio_key"]
+
+# Initialize an Adafruit IO HTTP API object
+io = IO_HTTP(aio_username, aio_key, requests)
+
+try:
+    # Get the 'temperature' feed from Adafruit IO
+    temperature_feed = io.get_feed("temperature")
+except AdafruitIO_RequestError:
+    # If no 'temperature' feed exists, create one
+    temperature_feed = io.create_new_feed("temperature")
+
+# Send random integer values to the feed
+temperature = sensor.temperature
+print("Sending {0} to temperature feed...".format(temperature))
+io.send_data(temperature_feed["key"], temperature)
+print("Data sent!")
